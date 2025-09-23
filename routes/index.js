7579cd9551953ab8a834b40f6df48aeb7f3ff3b9
Jaimie Bowen <jraebowen@gmail.com>
2025-09-23 15:16:33 -0700

    remove password length requirement and update 404 status code to utilize constant

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 const router = require("express").Router();
 const userRouter = require("./users");
 const itemRouter = require("./clothingItems");
+const ERROR_STATUS = require("../utils/errors");
 
 router.use("/users", userRouter);
 router.use("/items", itemRouter);
 
 router.use((req, res) => {
-  res.status(404).json({
+  res.status(ERROR_STATUS.NOT_FOUND).json({
     message: "Requested resource not found",
   });
 });
